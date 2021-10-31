e39a5658975289589778a2f128c8f953de7ed51d
Taras <tarasnurko84@gmail.com>
2021-10-31 18:57:18 +0300

    adapted second and third sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 import showMenu from "./modules/menu";
 
-showMenu('.menu__btn', 'menu__btn_active', '.menu__background', 'menu__background_active', '.menu__wrapper', 'menu__wrapper_active');
+showMenu('.menu__btn', 'menu__btn_active', '.menu__background', 'menu__background_active', '.menu__wrapper', 'menu__wrapper_active', 'overflow-hidden');
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,4 +1,4 @@
-export default function showMenu(btnSelector, btnActiveClass, menuBgSelector, menuBgActiveClass, menuWrapperSelector, menuWrapperActiveClass) {
+export default function showMenu(btnSelector, btnActiveClass, menuBgSelector, menuBgActiveClass, menuWrapperSelector, menuWrapperActiveClass, overflowClass) {
   let btn = document.querySelector(btnSelector),
       menuBg = document.querySelector(menuBgSelector),
       menuWrapper = document.querySelector(menuWrapperSelector);
@@ -8,10 +8,12 @@ export default function showMenu(btnSelector, btnActiveClass, menuBgSelector, me
       btn.classList.add(btnActiveClass);
       menuBg.classList.add(menuBgActiveClass);
       menuWrapper.classList.add(menuWrapperActiveClass);
+      document.body.classList.add(overflowClass);
     } else {
       btn.classList.remove(btnActiveClass);
       menuBg.classList.remove(menuBgActiveClass);
       menuWrapper.classList.remove(menuWrapperActiveClass);
+      document.body.classList.remove(overflowClass);
     }
   });
 }
